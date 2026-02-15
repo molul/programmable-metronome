8dcf1f936d72562469e4ac291ea2e0fe84648559
molul <luismorcillo@gmail.com>
2026-02-15 03:31:25 +0100

    Update MetronomeGrid.vue

--- src/components/MetronomeGrid.vue
+++ src/components/MetronomeGrid.vue
@@ -224,7 +224,7 @@ watch(
       <div
         v-for="r in rows"
         :key="`label-${r}`"
-        class="text-[10px] text-white flex items-center font-bold justify-end pr-2"
+        class="text-[10px] text-white flex items-end font-bold justify-end pr-2"
         :style="{
           width: tempoColumnWidth + 'px',
           height: cellH + 'px',
